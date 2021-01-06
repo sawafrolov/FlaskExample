02e268222c1afd5e632e6ae257d7001541a6399d
sawafrolov <sawa.frolov@yandex.ru>
2021-01-06 16:01:28 +0300

    Done second migration

--- db_migrate.py
+++ db_migrate.py
@@ -1,6 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db
+from app.models import User, Post
 
 migrate = Migrate(app, db)
 manager = Manager(app)
