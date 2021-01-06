6cff8b4c87360cb2bc2fa8daeff76dc15c0b4a3f
sawafrolov <sawa.frolov@yandex.ru>
2021-01-06 10:50:42 +0300

    Created first DB

---
+++ installations.bat
@@ -0,0 +1,11 @@
+pip install flask
+pip install flask-login
+pip install flask-openid
+pip install sqlalchemy
+pip install flask-sqlalchemy
+pip install sqlalchemy-migrate
+pip install flask-whooshalchemy
+pip install flask-wtf
+pip install pytz
+pip install flask-babel
+pip install flup
