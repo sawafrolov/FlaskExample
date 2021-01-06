43a5aaba83328c8efa1e86e9d229852d8540529e
sawafrolov <sawa.frolov@yandex.ru>
2021-01-06 10:04:21 +0300

    Hello, username!

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {
+        "nickname": "sawafrolov"
+    }
+    return render_template("index.html", title='Home', user=user)
