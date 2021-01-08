76222238f2b71ed1c595ff5df89ff748b7457ffb
sawafrolov <sawa.frolov@yandex.ru>
2021-01-08 08:09:42 +0300

    Added logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object("config")
@@ -8,3 +10,11 @@ db = SQLAlchemy(app)
 login = LoginManager()
 login.init_app(app)
 login.login_view = "login"
+
+if not app.debug:
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info("microblog startup")
