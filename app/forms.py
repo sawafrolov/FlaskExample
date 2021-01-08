d91c4ca65e27195d58d9c6e4275d7c91dd35359f
sawafrolov <sawa.frolov@yandex.ru>
2021-01-08 13:09:45 +0300

    Added follow and unfollow

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,10 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Le
 from app.models import User
 
 
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+
 class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
