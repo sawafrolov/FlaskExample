93f4c68951841f518c981ae3b96c59c6d4681667
sawafrolov <sawa.frolov@yandex.ru>
2021-01-08 05:29:06 +0300

    Added login procedure

--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,5 @@ app.config.from_object("config")
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = "login"
+oid = OpenID(app, os.path.join(basedir, "tmp"))
