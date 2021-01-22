5383d53e298f51a7868279af0a85d1a4c3291319
sawafrolov <sawa.frolov@yandex.ru>
2021-01-22 14:46:08 +0300

    Refactored auth routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -11,7 +11,7 @@ from app.auth.email import send_password_reset_email
 @bp.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -26,7 +26,7 @@ def register():
 @bp.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -35,20 +35,20 @@ def login():
             return redirect(url_for("auth.login"))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
-        return redirect(next_page or url_for("index"))
+        return redirect(next_page or url_for("main.index"))
     return render_template("auth/login.html", title=_("Sign In"), form=form)
 
 
 @bp.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for("index"))
+    return redirect(url_for("main.index"))
 
 
 @bp.route("/reset_password_request", methods=["GET", "POST"])
 def reset_password_request():
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
     form = ResetPasswordRequestForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -62,10 +62,10 @@ def reset_password_request():
 @bp.route("/reset_password/<token>", methods=["GET", "POST"])
 def reset_password(token):
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
     user = User.verify_reset_password_token(token)
     if not user:
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
     form = ResetPasswordForm()
     if form.validate_on_submit():
         user.set_password(form.password.data)
