7da66a2028b4b2cefecf87d8caa020a660cae3b4
sawafrolov <sawa.frolov@yandex.ru>
2021-01-27 14:58:07 +0300

    Refactored main routes

--- app/auth/reset_password.py
+++ app/auth/reset_password.py
@@ -3,9 +3,7 @@ from flask_babel import _
 from jwt import decode, encode
 from time import time
 from app.email import send_email
-from app.select_dao import SelectDAO
-
-secret_key = "you-shall-not-pass"
+from app.select_dao import select_user_by_id
 
 
 def get_reset_password_token(user, expires_in=600):
@@ -14,7 +12,7 @@ def get_reset_password_token(user, expires_in=600):
             'reset_password': user.id,
             'exp': time() + expires_in
         },
-        secret_key,
+        current_app.config["SECRET_KEY"],
         algorithm='HS256'
     )
 
@@ -34,9 +32,9 @@ def verify_reset_password_token(token):
     try:
         user_id = decode(
             token,
-            secret_key,
+            current_app.config["SECRET_KEY"],
             algorithms=['HS256']
         )['reset_password']
     except:
         return
-    return SelectDAO.select_user_by_id(user_id)
+    return select_user_by_id(user_id)
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import email_config
 
 LANGUAGES = ['en', 'ru']
 basedir = os.path.abspath(os.path.dirname(__file__))
+SECRET_KEY = "you-shall-not-pass"
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
