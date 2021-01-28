eca29bad436bb99966cf71627b317e80a78e3e6f
sawafrolov <sawa.frolov@yandex.ru>
2021-01-28 20:47:56 +0300

    Updated l10n

--- app/elasticsearch.py
+++ app/elasticsearch.py
@@ -1,7 +1,15 @@
+from requests import get, ConnectionError
 from elasticsearch import Elasticsearch
 
 
 def check_elasticsearch(url):
+    try:
+        response = get(url).json()
+    except ConnectionError:
+        return False
+    if "cluster_name" in response:
+        if response["cluster_name"] == "elasticsearch":
+            return True
     return False
 
 
