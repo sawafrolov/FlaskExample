387a5a70d4bcd7b267322069fa0d5cfde4d5460f
sawafrolov <sawa.frolov@yandex.ru>
2021-02-01 05:25:40 +0300

    Updated l10n

--- l10n_compile.py
+++
@@ -1,4 +0,0 @@
-from os import system as execute
-
-if execute('pybabel compile -d app/translations'):
-    raise RuntimeError('compile command failed')
--- l10n_update.py
+++
@@ -1,6 +0,0 @@
-from os import system as execute
-
-if execute('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
-    raise RuntimeError('extract command failed')
-if execute('pybabel update -i messages.pot -d app/translations'):
-    raise RuntimeError('update command failed')
